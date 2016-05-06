43ab9e4e95a55d76ef45641117e0f18ac709c422
Jeffrey Ling <jeffrey.r.ling@gmail.com>
2016-05-06 00:18:03 -0400

    untested training code

--- train_copy.lua
+++ train_copy.lua
@@ -189,8 +189,8 @@ function train(train_data, valid_data)
   local encoder_grad_proto2 = torch.zeros(valid_data.batch_l:max(), opt.max_sent_l, opt.rnn_size)
   context_proto = torch.zeros(valid_data.batch_l:max(), opt.max_sent_l, opt.rnn_size)
   context_proto2 = torch.zeros(valid_data.batch_l:max(), opt.max_sent_l, opt.rnn_size)
-  sent_conv_proto = torch.zeros(valid_data.batch_l:max(), opt.max_sent_l, opt.num_kernels)
-  sent_conv_grad_proto = torch.zeros(valid_data.batch_l:max(), opt.max_sent_l, opt.num_kernels) -- added by Jeffrey
+  sent_enc_proto = torch.zeros(valid_data.batch_l:max(), opt.max_sent_l, opt.num_kernels)
+  sent_enc_grad_proto = torch.zeros(valid_data.batch_l:max(), opt.max_sent_l, opt.num_kernels) -- added by Jeffrey
 
   -- clone encoder/decoder up to max source/target length   
   decoder_clones = clone_many_times(decoder, opt.max_sent_l)
@@ -221,8 +221,8 @@ function train(train_data, valid_data)
       context_proto2 = context_proto2:cuda()	 
     else
       context_proto = context_proto:cuda()
-      sent_conv_proto = sent_conv_proto:cuda()
-      sent_conv_grad_proto = sent_conv_grad_proto:cuda()
+      sent_enc_proto = sent_enc_proto:cuda()
+      sent_enc_grad_proto = sent_enc_grad_proto:cuda()
       encoder_grad_proto = encoder_grad_proto:cuda()	 
     end
   end
@@ -313,10 +313,11 @@ function train(train_data, valid_data)
       local batch_l, target_l, source_l = d[5], d[6], d[7]
 
       local encoder_grads = encoder_grad_proto[{{1, batch_l}, {1, source_l}}]
+      local sent_enc_grads = sent_enc_grads[{{1, batch_l}, {1, source_l}}] -- added by Jeffrey
 
       local rnn_state_enc = reset_state(init_fwd_enc, batch_l, 0)
       local context = context_proto[{{1, batch_l}, {1, source_l}}]
-      local sentence_enc = sentence_enc_proto[{{1, batch_l}, {1, source_l}}]
+      local sent_enc = sent_enc_proto[{{1, batch_l}, {1, source_l}}]
       if opt.gpuid >= 0 then
         cutorch.setDevice(opt.gpuid)
       end
@@ -324,10 +325,10 @@ function train(train_data, valid_data)
       -- forward prop encoder
       for t = 1, source_l do
         encoder_clones[t]:training()
-        local encoder_input = {source[t], table.unpack(rnn_state_enc[t-1])}
+        local s = sent_conv_model:forward(source[t]) -- create s_t once
+        sent_enc[{{},t}]:copy(s)
+        local encoder_input = {s, table.unpack(rnn_state_enc[t-1])}
         local out = encoder_clones[t]:forward(encoder_input)
-        sentence_enc[{{},t}]:copy(out[#out]) -- copy encoded sentence
-        table.remove(out) -- remove encoded sentence
         rnn_state_enc[t] = out
         context[{{},t}]:copy(out[#out])
       end
@@ -352,7 +353,7 @@ function train(train_data, valid_data)
       local decoder_input
       for t = 1, target_l do
         decoder_clones[t]:training()
-        local decoder_input = {sentence_enc[t], context[t], table.unpack(rnn_state_dec[t-1])}
+        local decoder_input = {sent_enc[t], context[t], table.unpack(rnn_state_dec[t-1])}
         local out = decoder_clones[t]:forward(decoder_input)
         local next_state = {}
         table.insert(preds, out[#out])
@@ -365,25 +366,27 @@ function train(train_data, valid_data)
 
       -- backward prop decoder
       encoder_grads:zero()	 
+      sent_enc_grads:zero()
       local drnn_state_dec = reset_state(init_bwd_dec, batch_l, 1)
       local loss = 0
       for t = target_l, 1, -1 do
-        local pred = preds[t]
+        local pred = preds[t] -- batch_l x 2
         loss = loss + criterion:forward(pred, target_out[t])/batch_l
         local dl_dpred = criterion:backward(pred, target_out[t])
         dl_dpred:div(batch_l)
-        --local dl_dtarget = generator:backward(preds[t], dl_dpred)
-        --drnn_state_dec[#drnn_state_dec]:add(dl_dtarget)
         drnn_state_dec[#drnn_state_dec]:add(dl_dpred)
-        local decoder_input = {sentence_enc[t], context[t], table.unpack(rnn_state_dec[t-1])}
+        local decoder_input = {sent_enc[t], context[t], table.unpack(rnn_state_dec[t-1])}
         local dlst = decoder_clones[t]:backward(decoder_input, drnn_state_dec)
         -- accumulate encoder/decoder grads
         encoder_grads[{{},t}]:add(dlst[2])
         drnn_state_dec[#drnn_state_dec]:zero()
-        drnn_state_dec[#drnn_state_dec]:add(dlst[3])
+        drnn_state_dec[#drnn_state_dec]:add(dlst[3]) -- backprop p_t
         for j = 4, #dlst do
           drnn_state_dec[j-3]:copy(dlst[j])
         end	    
+
+        -- accumulate for conv
+        sent_enc_grads[{{},t}]:add(dlst[1])
       end
       --word_vecs_dec.gradWeight[1]:zero()
       --if opt.fix_word_vecs_dec == 1 then
@@ -411,14 +414,24 @@ function train(train_data, valid_data)
       end
 
       for t = source_l, 1, -1 do
-        local encoder_input = {source[t], table.unpack(rnn_state_enc[t-1])}
+        local encoder_input = {sent_enc[t], table.unpack(rnn_state_enc[t-1])}
         drnn_state_enc[#drnn_state_enc]:add(encoder_grads[{{},t}])
         local dlst = encoder_clones[t]:backward(encoder_input, drnn_state_enc)
         for j = 1, #drnn_state_enc do
           drnn_state_enc[j]:copy(dlst[j+1])
         end	    
+
+        -- accumulate for conv
+        sent_enc_grads[{{},t}]:add(dlst[1])
       end
 
+      -- backprop sent_conv
+      for t = source_l, 1, -1 do
+        sent_conv_model:forward(source[t]) -- inefficient, but needed if not cloning
+        local dl_ds = sent_enc_grads[{{},t}]
+        sent_conv_model:backward(source[t], dl_ds)
+      end
+      -- fix word embeddings
       word_vecs_enc.gradWeight[1]:zero()
       if opt.fix_word_vecs_enc == 1 then
         word_vecs_enc.gradWeight:zero()
@@ -471,7 +484,8 @@ function train(train_data, valid_data)
 
   local total_loss, total_nonzeros, batch_loss, batch_nonzeros
   for epoch = opt.start_epoch, opt.epochs do
-    generator:training()
+    --generator:training()
+    sent_conv_model:training()
     if opt.num_shards > 0 then
       total_loss = 0
       total_nonzeros = 0	 
@@ -497,21 +511,24 @@ function train(train_data, valid_data)
     local savefile = string.format('%s_epoch%.2f_%.2f.t7', opt.savefile, epoch, score)      
     if epoch % opt.save_every == 0 then
       print('saving checkpoint to ' .. savefile)
-      clean_layer(encoder); clean_layer(decoder); clean_layer(generator)
-      torch.save(savefile, {{encoder, decoder, generator}, opt})
+      clean_layer(encoder); clean_layer(decoder); clean_layer(sent_conv_model) --clean_layer(generator)
+      --torch.save(savefile, {{encoder, decoder, generator}, opt})
+      torch.save(savefile, {{encoder, decoder, sent_conv_model}, opt})
     end
   end
   -- save final model
   local savefile = string.format('%s_final.t7', opt.savefile)
-  clean_layer(encoder); clean_layer(decoder); clean_layer(generator)
+  clean_layer(encoder); clean_layer(decoder); clean_layer(sent_conv_model) -- clean_layer(generator)
   print('saving final model to ' .. savefile)   
-  torch.save(savefile, {{encoder:double(), decoder:double(), generator:double()}, opt})   
+  --torch.save(savefile, {{encoder:double(), decoder:double(), generator:double()}, opt})   
+  torch.save(savefile, {{encoder:double(), decoder:double(), sent_conv_model:double()}, opt})   
 end
 
 function eval(data)
   encoder_clones[1]:evaluate()   
   decoder_clones[1]:evaluate() -- just need one clone
-  generator:evaluate()
+  sent_conv_model:evaluate()
+  --generator:evaluate()
   local nll = 0
   local total = 0
   for i = 1, data:size() do
@@ -520,13 +537,13 @@ function eval(data)
     local batch_l, target_l, source_l = d[5], d[6], d[7]
     local rnn_state_enc = reset_state(init_fwd_enc, batch_l, 1)
     local context = context_proto[{{1, batch_l}, {1, source_l}}]
-    local sentence_enc = sentence_enc_proto[{{1, batch_l}, {1, source_l}}]
+    local sent_enc = sent_enc_proto[{{1, batch_l}, {1, source_l}}]
     -- forward prop encoder
     for t = 1, source_l do
-      local encoder_input = {source[t], table.unpack(rnn_state_enc)}
+      local s = sent_conv_model:forward(source[t])
+      sent_enc[{{},t}]:copy(s)
+      local encoder_input = {s, table.unpack(rnn_state_enc)}
       local out = encoder_clones[1]:forward(encoder_input)
-      sentence_enc[{{},t}]:copy(out[#out]) -- copy encoded sentence
-      table.remove(out) -- remove encoded sentence
       rnn_state_enc = out
       context[{{},t}]:copy(out[#out])
     end
@@ -547,7 +564,7 @@ function eval(data)
     end      
     local loss = 0
     for t = 1, target_l do
-      local decoder_input = {target[t], context, table.unpack(rnn_state_dec)}
+      local decoder_input = {sent_enc[t], context[t], table.unpack(rnn_state_dec)}
       local out = decoder_clones[1]:forward(decoder_input)
       rnn_state_dec = {}
       table.insert(rnn_state_dec, out[#out])
@@ -625,7 +642,7 @@ function main()
   if opt.train_from:len() == 0 then
     encoder = make_lstm(valid_data, opt, 'enc')
     decoder = make_lstm(valid_data, opt, 'dec')
-    sent_conv = make_sent_conv(valid_data, opt) -- added by Jeffrey
+    sent_conv_model = make_sent_conv(valid_data, opt) -- added by Jeffrey
     criterion = make_criterion(opt)
   else
     assert(path.exists(opt.train_from), 'checkpoint path invalid')
@@ -638,7 +655,7 @@ function main()
     criterion = make_criterion(opt)
   end   
 
-  layers = {encoder, decoder, sent_conv}
+  layers = {encoder, decoder, sent_conv_model}
 
   if opt.gpuid >= 0 then
     for i = 1, #layers do	 
@@ -659,7 +676,7 @@ function main()
 
   encoder:apply(get_layer)   
   decoder:apply(get_layer)   
-  sent_conv:apply(get_layer)
+  sent_conv_model:apply(get_layer)
 
   train(train_data, valid_data)
 end
